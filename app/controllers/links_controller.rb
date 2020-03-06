e71266878f573e65b5af7f0a5fe6a7f42abec37a
railkun <111naruto111@gmail.com>
2020-03-06 17:08:53 +0200

    create service

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,38 @@
+class LinksController < ApplicationController
+
+  def index
+    @links = Link.all
+  end
+
+  def show
+    @link = Link.find(params[:id])
+  end
+
+  def new
+    @link = Link.new
+  end
+
+  def create
+    @link = Link.create(link_params)
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    @link.update(link_params)
+  end
+
+  def destroy
+    @link = Link.find(params[:id])
+    @link.destroy
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:link, :link_descrition)
+  end
+end
---
+++ db/migrate/20200306135033_create_links.rb
@@ -0,0 +1,10 @@
+class CreateLinks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :links do |t|
+      t.string :user_id
+      t.string :link
+      t.string :link_descrition
+      t.timestamps
+    end
+  end
+end
